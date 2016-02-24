bdb2d02c0b369e2ced9721fe3879148296754a92
Gregory Brewster <gbrewster@pittock-01.agoahosting.com>
2016-02-24 13:42:43 -0800

    pushing initial files

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * Calix Consumer Connect Variables
+ */
+
+$config['cc-host-uri'] = "https://gcs.calix.com:8444";
+$config['cc-username'] = "huehuehue-cc-username";
+$config['cc-password'] = "rbrbbrbrbr-cc-password";
---
+++ php/cc-pull-from-serno.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Intranet
+ *
+ *
+ * @package    SandyNet Intranet
+ * @author     Gregory Brewster <gbrewster@agoasite.com>
+ * @copyright  (C)2016 City of Sandy
+ *
+ */
+
+include_once("../config.php");
+
+// CC is case sensitive for serial numbers!
+$serno = strtoupper($_GET['serno']);
+
+if(strlen($serno) == 6){
+	$serialNumber = 'CXNK00'.$serno;
+} else {
+	$serialNumber = $serno;
+}
+
+$header_options = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Authorization: Basic " . base64_encode($config['cc-username'].":".$config['cc-password'])." \r\n"
+  )
+);
+
+$context = stream_context_create($header_options);
+
+// Open the file using the HTTP headers set above
+$file = file_get_contents($config['cc-host-uri'].'/api/device?serialNumber='.$serialNumber, false, $context);
+
+
+// Two types of outputs
+print $file;
+
+// or decode to PHP object
+print_r(json_decode($file));
+?>
+
